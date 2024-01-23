d4fd70bcf7c5970ba89997b9ab271e58ab71aba5
Sam Blackwell <SamFiBlackwell@gmail.com>
2024-01-23 14:50:31 -0500

    Ajout du Footer

---
+++ frontend/src/composants/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => {
+    return (
+        <footer className='container-fluid position-sticky bottom-0 py-2 bg-secondary text-white'>
+            <p className='m-0 text-center'>La musique &copy; 2024</p>
+        </footer>
+    );
+}
