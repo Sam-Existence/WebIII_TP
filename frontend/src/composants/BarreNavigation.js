3924dc49bdcc9502e06a744a463846a2784bf07f
Sam Blackwell <SamFiBlackwell@gmail.com>
2024-01-23 15:03:04 -0500

    Ajout de BarreNavigation

---
+++ frontend/src/composants/BarreNavigation.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+    Navbar,
+    Nav,
+    Container
+} from 'react-bootstrap';
+
+export const BarreNavigation = () => {
+    return (
+        <Navbar bg="secondary" data-bs-theme="dark" expand="sm">
+            <Container>
+                <Navbar.Brand href="/">La musique</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Accueil</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
