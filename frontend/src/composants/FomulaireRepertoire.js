8576efc3a31adc25481926d6819a882522bfa4ed
Sam-Existence <SamFiBlackwell@gmail.com>
2024-02-06 11:52:56 -0500

    Modification de la structure de FormulaireRepertoire

--- frontend/src/composants/FomulaireRepertoire.js
+++ frontend/src/composants/FomulaireRepertoire.js
@@ -2,11 +2,10 @@ import { React, useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-export const FomulaireRepertoire = ({ type="post", repertoire=null}) => {
+export const FomulaireRepertoire = ({ handleSubmit, status="", repertoire=null}) => {
     const [titre, setTitre] = useState('');
     const [artiste, setArtiste] = useState('');
     const [categorie, setCategorie] = useState('');
-    const [status, setStatus] = useState('');
 
     useEffect( () => {
         setTitre(repertoire === null ? '' : repertoire.titre);
@@ -14,57 +13,13 @@ export const FomulaireRepertoire = ({ type="post", repertoire=null}) => {
         setCategorie(repertoire === null ? '' : repertoire.categorie);
     }, [repertoire]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const body = 
-            {
-                id: (repertoire === null ? null : repertoire._id),
-                titre,
-                artiste,
-                categorie
-            };
-        let reponse;
-        switch (type) {
-            case "post":
-                reponse = await fetch('/api/repertoire/pieces', {
-                    method: 'POST',
-                    body: JSON.stringify(body),
-                    headers: { 'Content-Type': 'application/json' }
-                });
-                break;
-            case "put": 
-                reponse = await fetch(`/api/repertoire/pieces/${repertoire._id}`, {
-                    method: 'PUT',
-                    body: JSON.stringify(body),
-                    headers: { 'Content-Type': 'application/json' }
-                });
-                break;
-            
-            default:
-                break;
-        }
-
-        if (reponse.status === 201 || reponse.status === 200) {
-            const resultat = await reponse.json();
-            
-            switch (type) {
-                case "post":
-                    setStatus(`Le répertoire a été ajouté avec l'id : ${resultat.location.split('/')[3]}`)
-                    break;
-                
-                case "put":
-                    setStatus(`Le répertoire ${repertoire._id} a été mis à jour`);
-                    break;
-                
-                default:
-                    break;
-            }
-        } else {
-            setStatus("Erreur, le répertoire n'a pas été ajouté");
-        }
-    }
     return (
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={(e) => handleSubmit(e, {
+            id: (repertoire === null ? null : repertoire._id),
+            titre,
+            artiste,
+            categorie
+        })}>
             <Form.Group className="mb-3" controlId="titre">
                 <Form.Label>Titre</Form.Label>
                 <Form.Control type="text" value={titre} onChange={(e) => setTitre(e.target.value)} />
--- frontend/src/pages/PageAjouterRepertoire.js
+++ frontend/src/pages/PageAjouterRepertoire.js
@@ -1,12 +1,32 @@
-import React from "react";
+import { React, useState } from "react";
 
 import { FomulaireRepertoire } from "../composants/FomulaireRepertoire";
 
 export const PageAjouterRepertoire = () => {
+    const [status, setStatus] = useState('');
+
+    const handleSubmit = async (e, repertoire) => {
+        e.preventDefault();
+
+        let reponse = await fetch('/api/repertoire/pieces', {
+            method: 'POST',
+            body: JSON.stringify(repertoire),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (reponse.status === 201) {
+            const resultat = await reponse.json();
+
+            setStatus(`Le répertoire a été ajouté avec l'id : ${resultat.location.split('/')[3]}`);
+        } else {
+            setStatus("Erreur, le répertoire n'a pas été ajouté");
+        }
+    }
+
     return (
         <main>
             <h2 className="text-center">Ajouter un répertoire</h2>
-            <FomulaireRepertoire />
+            <FomulaireRepertoire handleSubmit={handleSubmit} status={status}/>
         </main>
     );
 }
