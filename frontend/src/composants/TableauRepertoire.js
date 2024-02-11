7a7884453a96630cb2a23fad8b0368fc67925799
Sam Blackwell <SamFiBlackwell@gmail.com>
2024-02-11 14:46:23 -0500

    Ajout du triage au TableauRepertoire

--- frontend/src/composants/TableauRepertoire.js
+++ frontend/src/composants/TableauRepertoire.js
@@ -1,24 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from 'react-bootstrap/Table';
 import { RangeeRepertoire } from "./RangeeRepertoire";
+import { SlArrowDown, SlArrowUp } from 'react-icons/sl';
 
 export const TableauRepertoire = ({ repertoires, buttons=false }) => {
-    if (!repertoires?.length) {
-        return;
+    const [repertoiresTries, setRepertoiresTries] = useState(repertoires);
+    if (repertoires && !repertoiresTries) {
+        setRepertoiresTries(repertoires.sort((a, b) => a.categorie.localeCompare(b.categorie)));
+    }
+
+    const trierParTitreCroissant = () => {
+        const nouveauRepertoires = repertoiresTries.toSorted((a, b) => a.titre.localeCompare(b.titre));
+        setRepertoiresTries(nouveauRepertoires);
+    }
+
+    const trierParTitreDecroissant = () => {
+        const nouveauRepertoires = repertoiresTries.toSorted((a, b) => b.titre.localeCompare(a.titre));
+        setRepertoiresTries(nouveauRepertoires);
+    }
+
+    const trierParArtisteCroissant = () => {
+        const nouveauRepertoires = repertoiresTries.toSorted((a, b) => a.artiste.localeCompare(b.artiste));
+        setRepertoiresTries(nouveauRepertoires);
+    }
+
+    const trierParArtisteDecroissant = () => {
+        const nouveauRepertoires = repertoiresTries.toSorted((a, b) => b.artiste.localeCompare(a.artiste));
+        setRepertoiresTries(nouveauRepertoires);
+    }
+
+    const trierParCategorieCroissant = () => {
+        const nouveauRepertoires = repertoiresTries.toSorted((a, b) => a.categorie.localeCompare(b.categorie));
+        setRepertoiresTries(nouveauRepertoires);
+    }
+
+    const trierParCategorieDecroissant = () => {
+        const nouveauRepertoires = repertoiresTries.toSorted((a, b) => b.categorie.localeCompare(a.categorie));
+        setRepertoiresTries(nouveauRepertoires);
     }
 
     return(
         <Table striped bordered hover>
             <thead>
                 <tr>
-                    <th>Titre</th>
-                    <th>Artiste</th>
-                    <th>Catégorie</th>
+                    <th>
+                        <div className="d-flex justify-content-between">
+                            <div className="p-2">Titre</div>
+                            <div className="p-2">
+                                <SlArrowUp onClick={trierParTitreCroissant}/>
+                                <SlArrowDown onClick={trierParTitreDecroissant}/>
+                            </div>
+                        </div>
+                    </th>
+                    <th>
+                        <div className="d-flex justify-content-between">
+                            <div className="p-2">Artiste</div>
+                            <div className="p-2">
+                                <SlArrowUp onClick={trierParArtisteCroissant}/>
+                                <SlArrowDown onClick={trierParArtisteDecroissant}/>
+                            </div>
+                        </div>
+                    </th>
+                    <th>
+                        <div className="d-flex justify-content-between">
+                            <div className="p-2">Catégorie</div>
+                            <div className="p-2">
+                                <SlArrowUp onClick={trierParCategorieCroissant}/>
+                                <SlArrowDown onClick={trierParCategorieDecroissant}/>
+                            </div>
+                        </div>
+                    </th>
                     {buttons ? <th></th> : <></>}
                 </tr>
             </thead>
             <tbody>
-                {repertoires.map(repertoire => 
+                {repertoiresTries?.map(repertoire => 
                     <RangeeRepertoire 
                         key={repertoire._id}
                         repertoire={repertoire}
