438ecb201638145f3f6564e4583b9e9d585449cb
Félix Gason <isatis.snowfox@gmail.com>
2024-02-11 15:54:21 -0500

    Ajout du début du panneau de filtrage du répertoire

---
+++ frontend/src/composants/PanneauFiltrerRepertoire.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Dropdown } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+
+export const PanneauFiltrerDemandesSpeciales = ({demandesSpeciales}) => {
+    return (
+        <Form>
+            <Dropdown id="categories">
+                { demandesSpeciales.map(demandeSpeciale => 
+                    <Form.Check 
+                        id={demandeSpeciale._id} 
+                        key={demandeSpeciale._id} 
+                        type="checkbox" 
+                        label={demandeSpeciale.nom} />
+                ) }
+            </Dropdown>
+        </Form>
+    );
+};
