03890dd9dfe69453e68cff75054d4a29014bfbc3
Félix Gason <isatis.snowfox@gmail.com>
2024-02-11 22:29:10 -0500

    Correction des catégories pour les pièces de demandes spéciales

--- frontend/src/composants/ListePiecesDemandeSpeciale.js
+++ frontend/src/composants/ListePiecesDemandeSpeciale.js
@@ -6,45 +6,71 @@ import { Button } from "react-bootstrap";
 import { RangeePieceDemandeSpeciale } from "./RangeePieceDemandeSpeciale";
 
 export const ListePiecesDemandeSpeciale = ({ pieces, typeBouton="supprimer", handleModification }) => {
-    const [piecesTries, setPiecesTries] = useState(pieces);
-
-    useEffect(() => {
-        setPiecesTries(pieces?.sort((a, b) => a.categorie.localeCompare(b.categorie)));
-    }, [pieces]);
+    const [repertoiresTries, setRepertoiresTries] = useState();
+    const [repertoireNonAplati, setRepertoireNonAplati] = useState();
+    const [repertoiresAvecCategoriesAplaties, setRepertoiresAvecCategoriesAplaties ] = useState();
+    
+    
 
     const handleSupression = repertoireASupprimer => {
-        const nouveauRepertoires = piecesTries.filter(repertoire => repertoire._id !== repertoireASupprimer._id);
-        setPiecesTries(nouveauRepertoires);
+        const nouveauRepertoires = repertoiresTries.filter(repertoire => repertoire._id !== repertoireASupprimer._id);
+        setRepertoiresTries(nouveauRepertoires);
     }
 
     const trierParTitreCroissant = () => {
-        const nouveauRepertoires = piecesTries.toSorted((a, b) => a.titre.localeCompare(b.titre));
-        setPiecesTries(nouveauRepertoires);
+        const nouveauRepertoires = repertoireNonAplati.toSorted((a, b) => a.titre.localeCompare(b.titre));
+        setRepertoiresTries(nouveauRepertoires);
     }
 
     const trierParTitreDecroissant = () => {
-        const nouveauRepertoires = piecesTries.toSorted((a, b) => b.titre.localeCompare(a.titre));
-        setPiecesTries(nouveauRepertoires);
+        const nouveauRepertoires = repertoireNonAplati.toSorted((a, b) => b.titre.localeCompare(a.titre));
+        setRepertoiresTries(nouveauRepertoires);
     }
 
     const trierParArtisteCroissant = () => {
-        const nouveauRepertoires = piecesTries.toSorted((a, b) => a.artiste.localeCompare(b.artiste));
-        setPiecesTries(nouveauRepertoires);
+        const nouveauRepertoires = repertoireNonAplati.toSorted((a, b) => a.artiste.localeCompare(b.artiste));
+        setRepertoiresTries(nouveauRepertoires);
     }
 
     const trierParArtisteDecroissant = () => {
-        const nouveauRepertoires = piecesTries.toSorted((a, b) => b.artiste.localeCompare(a.artiste));
-        setPiecesTries(nouveauRepertoires);
+        const nouveauRepertoires = repertoireNonAplati.toSorted((a, b) => b.artiste.localeCompare(a.artiste));
+        setRepertoiresTries(nouveauRepertoires);
     }
 
     const trierParCategorieCroissant = () => {
-        const nouveauRepertoires = piecesTries.toSorted((a, b) => a.categorie.localeCompare(b.categorie));
-        setPiecesTries(nouveauRepertoires);
+        const nouveauRepertoires = repertoiresAvecCategoriesAplaties.toSorted((a, b) => a.categories[0].localeCompare(b.categories[0]));
+        setRepertoiresTries(nouveauRepertoires);
     }
 
     const trierParCategorieDecroissant = () => {
-        const nouveauRepertoires = piecesTries.toSorted((a, b) => b.categorie.localeCompare(a.categorie));
-        setPiecesTries(nouveauRepertoires);
+        const nouveauRepertoires = repertoiresAvecCategoriesAplaties.toSorted((a, b) => b.categories[0].localeCompare(a.categories[0]));
+        setRepertoiresTries(nouveauRepertoires);
+    }
+
+    if (pieces && !repertoireNonAplati) {
+        setRepertoireNonAplati(pieces.sort((a, b) => a.categories[0].localeCompare(b.categories[0])));
+    }
+
+    if (pieces && !repertoiresTries) {
+        let nouveauRepertoiresAvecCategoriesAplaties = pieces.flatMap((repertoire) => {
+            let repertoireEtendu = [];
+            repertoire.categories.forEach((categorie, index) => {
+                repertoireEtendu.push(
+                    {
+                        _id: repertoire._id, 
+                        titre: repertoire.titre, 
+                        artiste: repertoire.artiste, 
+                        categories: [repertoire.categories[index]]
+                    }
+                );
+            });
+            return repertoireEtendu;
+        });
+        setRepertoiresAvecCategoriesAplaties(nouveauRepertoiresAvecCategoriesAplaties);
+        
+    }
+    if (repertoiresAvecCategoriesAplaties && !repertoiresTries) {
+        trierParCategorieCroissant();
     }
 
     return(
@@ -82,9 +108,9 @@ export const ListePiecesDemandeSpeciale = ({ pieces, typeBouton="supprimer", han
                 </tr>
             </thead>
             <tbody>
-                {piecesTries?.map(repertoire => 
+                {repertoiresTries?.map(repertoire => 
                     <RangeePieceDemandeSpeciale 
-                        key={repertoire._id}
+                        key={repertoire._id + repertoire.categories[0]}
                         piece={repertoire}
                         typeBouton={typeBouton}
                         handleModification={handleModification}
--- frontend/src/composants/RangeePieceDemandeSpeciale.js
+++ frontend/src/composants/RangeePieceDemandeSpeciale.js
@@ -4,6 +4,9 @@ import { Button } from "react-bootstrap";
 
 export const RangeePieceDemandeSpeciale = ({ piece, typeBouton = "supprimer", handleModification }) => {
     let bouton;
+    const categories = piece.categories.reduce((acc, categorie, index) => 
+        acc + ((piece.categories.length <= index + 1) ? categorie : `${categorie}, `)
+    , "");
     switch (typeBouton) {
         case "supprimer":
             bouton =
@@ -35,7 +38,7 @@ export const RangeePieceDemandeSpeciale = ({ piece, typeBouton = "supprimer", ha
         <tr>
             <td>{piece.titre}</td>
             <td>{piece.artiste}</td>
-            <td>{piece.categorie}</td>
+            <td>{categories}</td>
             <td>{bouton}</td>
         </tr>
     )
